32daf39d77cfd693f48fcf2ed8675b722bb1c702
natacha romano <natacharomanonr@gmail.com>
2019-05-20 11:07:54 -0300

    refactoring bhaskara and fizzbuzz

--- bhaskara.py
+++ bhaskara.py
@@ -1,24 +1,26 @@
 import math
 
-a = float(input('Digite o primeiro número: '))
-b = float(input('Digite o segundo número: '))
-c = float(input('Digite o terceiro número: '))
+def delta(a, b, c):
+    return b ** 2 - 4 * a * c
 
-delta = b ** 2 - 4 * a * c
+def main():
+    a_input = float(input('Digite o primeiro número: '))
+    b_input = float(input('Digite o segundo número: '))
+    c_input = float(input('Digite o terceiro número: '))
+    imprime_raizes(a_input, b_input, c_input)
 
-if delta == 0:
-    raiz1 = (-b + math.sqrt(delta)) / (2 * a)
-    print('a raiz desta equação é', raiz1)
-else:
-    if delta < 0:
-        print('esta equação não possui raízes reais')
+def  imprime_raizes(a, b, c):
+    d = delta(a, b, c)
+    if d == 0:
+        raiz1 = (-b + math.sqrt(d)) / (2 * a)
+        print('a raiz desta equação é', raiz1)
     else:
-        raiz1 = (-b + math.sqrt(delta)) / (2 * a)
-        raiz2 = (-b - math.sqrt(delta)) / (2 * a)
-        if raiz1 > raiz2:
-            print('as raízes da equação são', raiz2, 'e', raiz1)
+        if d < 0:
+        print('esta equação não possui raízes reais')
         else:
-            print('as raízes da equação são', raiz1, 'e', raiz2)
+            raiz1 = (-b + math.sqrt(delta)) / (2 * a)
+            raiz2 = (-b - math.sqrt(delta)) / (2 * a)
+            print('as raízes da equação são', raiz2, 'e', raiz1)
 
 
         
